1a223e741590591214c6c7848d7861d7d292a7c6
Brandon Chin <icefence@me.com>
2019-02-15 14:36:33 -0500

    can read in file contents and display, as well as sort of generate ngrams? need to test further

---
+++ ngram.py
@@ -0,0 +1,29 @@
+import re
+# import sys
+
+# f = open(sys.argv[1],"r")
+# contents = f.read()
+# f.close()
+# print contents
+
+
+def generate_ngrams(s, n):
+    # Convert to lowercases
+    s = s.lower()
+
+    # Replace all none alphanumeric characters with spaces
+    s = re.sub(r'[^a-zA-Z0-9\s]', ' ', s)
+
+    # Break sentence in the token, remove empty tokens
+    tokens = [token for token in s.split(" ") if token != ""]
+
+    # Use the zip function to help us generate n-grams
+    # Concatentate the tokens into ngrams and return
+    ngrams = zip(*[tokens[i:] for i in range(n)])
+    return [" ".join(ngram) for ngram in ngrams]
+
+contents = "Natural-language processing (NLP) is an area of computer science " \
+    "and artificial intelligence concerned with the interactions " \
+    "between computers and human (natural) languages."
+
+print (generate_ngrams(contents, n=5))
