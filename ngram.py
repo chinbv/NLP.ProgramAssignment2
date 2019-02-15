0ecc2534373b3941e9a4ce90e82dbdecbe8213e6
Brandon Chin <icefence@me.com>
2019-02-15 15:00:34 -0500

    removed table of contents from books and removed one book. Printing out number of tokens as well

--- ngram.py
+++ ngram.py
@@ -17,13 +17,21 @@ def generate_ngrams(s, n):
     # Break sentence in the token, remove empty tokens
     tokens = [token for token in s.split(" ") if token != ""]
 
+    count=0
+
+    for x in tokens:
+        count+=1
+        print 'Token Count = ' + str(count)
+
     # Use the zip function to help us generate n-grams
     # Concatentate the tokens into ngrams and return
     ngrams = zip(*[tokens[i:] for i in range(n)])
     return [" ".join(ngram) for ngram in ngrams]
 
-contents = "Natural-language processing (NLP) is an area of computer science " \
-    "and artificial intelligence concerned with the interactions " \
-    "between computers and human (natural) languages."
+# contents = "Natural-language processing (NLP) is an area of computer science " \
+#     "and artificial intelligence concerned with the interactions " \
+#     "between computers and human (natural) languages."
+
+contents = "my, oh my, i wish i had 100 dollars." \
 
 print (generate_ngrams(contents, n=5))
